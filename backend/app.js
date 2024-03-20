597eaa0de46d2ba42a529f9682c900bfe1c8793a
Sajidd0 <l191237@lhr.nu.edu.pk>
2024-03-20 21:09:23 +0500

    deployment changes 2

--- backend/app.js
+++ backend/app.js
@@ -5,73 +5,122 @@ import 'dotenv/config'
 const app = express();
 app.use(cors());
 app.use(express.json());
-const db=mysql.createConnection({
-    host:process.env.DB_HOST ,
-    user:process.env.DB_USER,
-    password:process.env.DB_PASSWORD,
-    database:process.env.DB_DATABASE
-})
-app.post("/alltasks/:ip",(req,res)=>{
+const url='mysql://root:LrDtnRiwkULYttHRdLDmgbsjVIVIdRnE@monorail.proxy.rlwy.net:3306/railway';
+async function connectToDatabase() {
+    try {
+      const connection = await mysql.createConnection(url);
+      return connection;
+    } catch (err) {
+      console.error('error connecting:', err);
+      throw err;
+    }
+  }
+app.post("/alltasks/:ip",async (req,res)=>{
     const q="SELECT * FROM tasks WHERE ip=?";
-    const values=[req.params.ip]
-    db.query(q,[values],(err,data)=>{
-        if(err){
-            return res.json(err);
-        }
-        else
-        {
-            return res.json(data);
-        }
-    })
+    const db = await connectToDatabase();
+    console.log("Connection made");
+    try
+    {
+        const values=[req.params.ip]
+        db.query(q,[values],(err,data)=>{
+            if (err) {
+                return res.status(401).json({ success: false, error: err.message });
+            }
+            else
+            {
+                return res.json(data);
+            }
+        })
+    }
+    catch(err)
+    {
+        console.log("Error in Connection ")
+    }
+    finally{
+        db.end();
+    }
+   
 })
-app.post("/add",(req,res)=>{
+app.post("/add",async (req,res)=>{
     const q="INSERT INTO tasks (`shortdes`, `date`, `longdes`, `assignedto`,`ip`) VALUES(?)";
-    const obj=new Date();
-    const date=obj.getDate()+"/"+obj.getMonth()+"/"+obj.getFullYear();
-    const values=[
-        req.body.shortdes,
-        date,
-        req.body.longdes,
-        req.body.assignedto,
-        req.body.ip,
-    ]
-    db.query(q,[values],(err,data)=>{
-        if(err){
-            return res.json(err);
-        }
-        else
-        {
-            return res.json(data);
-        }
-    })
+    const db = await connectToDatabase();
+    try
+    {
+        const obj=new Date();
+        const date=obj.getDate()+"/"+obj.getMonth()+"/"+obj.getFullYear();
+        const values=[
+            req.body.shortdes,
+            date,
+            req.body.longdes,
+            req.body.assignedto,
+            req.body.ip,
+        ]
+        db.query(q,[values],(err,data)=>{
+            if (err) {
+                return res.status(401).json({ success: false, error: err.message });
+            }
+            else
+            {
+                return res.json(data);
+            }
+        })
+    }
+    catch(err)
+    {
+        console.log("Error in Connection ")
+    }
+    finally{
+         db.end();
+    }
 })
-app.delete("/delete/:id",(req,res)=>{
+app.delete("/delete/:id",async(req,res)=>{
     const q="DELETE FROM tasks WHERE id = ?";
-    const values=[req.params.id]
-    db.query(q,[values],(err,data)=>{
-        if(err){
-            return res.json(err);
-        }
-        else
-        {
-            return res.json(data);
-        }
-    })
+    const db = await connectToDatabase();
+    try
+    {
+        const values=[req.params.id]
+        db.query(q,[values],(err,data)=>{
+            if (err) {
+                return res.status(401).json({ success: false, error: err.message });
+            }
+            else
+            {
+                return res.json(data);
+            }
+        })
+    }
+    catch(err)
+    {
+        console.log("Error in Connection ")
+    }
+    finally{
+         db.end();
+    }
 })
-app.put("/update",(req,res)=>{
+app.put("/update",async(req,res)=>{
     const obj=new Date();
     const date=obj.getDate()+"/"+obj.getMonth()+"/"+obj.getFullYear();
     const q=`UPDATE tasks SET shortdes = '${req.body.shortdes}' , longdes = '${req.body.longdes}' , date = '${date}' , assignedto = '${req.body.assignedto}' ,modified = 1 WHERE id = ${req.body.id}`;
-   
-    db.query(q,(err,data)=>{
-        if(err){
-            return res.json(err);
-        }
-        else
-        {
-            return res.json(data);
-        }
-    })
+    const db = await connectToDatabase();
+    try
+    {
+        db.query(q,(err,data)=>{
+            if (err) {
+                return res.status(401).json({ success: false, error: err.message });
+            }
+            else
+            {
+                return res.json(data);
+            }
+        })
+    }
+    catch(err)
+    {
+        console.log("Error in Connection ")
+    }
+    finally{
+         db.end();
+    }
 })
 const PORT = process.env.PORT
 app.listen(PORT,()=>{
